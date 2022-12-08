8e4afb77520ee038e97d2011af68437e71368928
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-08 14:23:21 +0530

    add

--- src/main/java/com/nest/studentappnew_backend/controller/StudentController.java
+++ src/main/java/com/nest/studentappnew_backend/controller/StudentController.java
@@ -1,24 +1,31 @@
 package com.nest.studentappnew_backend.controller;
 
+import com.nest.studentappnew_backend.dao.StudentDao;
 import com.nest.studentappnew_backend.model.Students;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class StudentController {
-    @GetMapping("/")
-    public String Homepage(){
-        return "welcome to my website";
-    }
 
-    @PostMapping(path = "/add",consumes = "application/json" ,produces = "application/json")
-    public  String AddStudent(@RequestBody Students s){
+    @Autowired
+    private StudentDao dao;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public Map<String, String> AddStudent(@RequestBody Students s) {
         System.out.println(s.getName().toString());
-        return "Student added successfully";
+        System.out.println(s.getAdmno().toString());
+        System.out.println(s.getRollno());
+        System.out.println(s.getCollege().toString());
+        dao.save(s);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
+
+
     }
-
-
-
 }
